90deb55de683da547899faefab587727f50f03a5
cjl72 <55719709+cjl72@users.noreply.github.com>
2021-03-22 18:14:48 -0400

    Task: Initialize Post: model, migration, factory, and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
         \App\Models\Page::factory(10)->create();
+        \App\Models\Post::factory(10)->create();
+
     }
 }
