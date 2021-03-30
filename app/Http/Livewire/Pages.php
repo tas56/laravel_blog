7c499b5b5e3621afbb9745a7b393fb62603d1d5f
cjl72 <55719709+cjl72@users.noreply.github.com>
2021-03-29 20:00:48 -0400

    Task: Created Pages livewire component

---
+++ app/Http/Livewire/Pages.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Page;
+use App\Models\User;
+
+class Pages extends Component
+{
+    public $pages, $title, $body, $page_id;
+    public $isOpen = 0;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public function render()
+    {
+        $user = auth()->user();
+        $this->pages = $user->pages;
+        return view('livewire.posts');
+    }
+    public function index() {
+
+        return view('livewire.posts_public');
+    }
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public function create()
+    {
+        $this->resetInputFields();
+        $this->openModal();
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public function openModal()
+    {
+        $this->isOpen = true;
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public function closeModal()
+    {
+        $this->isOpen = false;
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    private function resetInputFields(){
+        $this->title = '';
+        $this->body = '';
+        $this->post_id = '';
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public function store()
+    {
+        $this->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        Post::updateOrCreate(['id' => $this->post_id], [
+            'title' => $this->title,
+            'body' => $this->body,
+        ]);
+
+        session()->flash('message',
+            $this->page_id ? 'Page Updated Successfully.' : 'Page Created Successfully.');
+
+        $this->closeModal();
+        $this->resetInputFields();
+    }
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public function edit($id)
+    {
+        $page = Page::findOrFail($id);
+        $this->page_id = $id;
+        $this->title = $page->title;
+        $this->body = $page->body;
+
+        $this->openModal();
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public function delete($id)
+    {
+        Page::find($id)->delete();
+        session()->flash('message', 'Page Deleted Successfully.');
+    }
+}
