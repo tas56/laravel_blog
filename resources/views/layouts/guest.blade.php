65ed70dbe1084ee1f109b43d63bbf99c652adbe6
Thomas Semiz <tas56@njit.edu>
2021-04-27 14:49:09 -0400

    Navigation styling

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'The Connecting Humanity Project') }}</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -17,23 +17,59 @@
     <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 <body>
-<div class="min-h-0 bg-gray-100">
-    <div class="relative flex items-top justify-center min-h-0 bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+<div class="min-h-0 bg-teal-500">
         @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                @auth
-                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
-                @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+            <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+                <div class="flex items-center flex-shrink-0 text-black mr-6">
+                    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+                    <span class="font-semibold text-xl tracking-tight">The Connecting Humanity Project</span>
+                </div>
+                <div class="block lg:hidden">
+                    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                    </button>
+                </div>
+                <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                    <div class="text-sm lg:flex-grow">
+                        <a href="{{ route('home') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-gray-500 mr-4">
+                            Home
+                        </a>
+                        <a href="{{ route('public_pages_index') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-gray-500 mr-4">
+                            Pages
+                        </a>
+                        <a href="{{ route('public_posts_index') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-gray-500">
+                            Posts
+                        </a>
+                    </div>
+                    <div>
+                        @auth
+                            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                        @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                    @endif
-                @endauth
-                <a href="{{ route('public_pages_index') }}" class="text-sm text-gray-700 underline">Pages</a>
-                <a href="{{ route('public_posts_index') }}" class="text-sm text-gray-700 underline">Posts</a>
-                <a href="{{ route('home') }}" class="text-sm text-gray-700 underline">Home</a>
-            </div>
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            @endif
+                        @endauth
+                        <a href="#" class="ml-4 inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Donate !</a>
+                        </div>
+                </div>
+            </nav>
+
+{{--            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">--}}
+{{--                <a href="{{ route('home') }}" class="text-sm text-gray-700 underline">Home</a>--}}
+{{--                <a href="{{ route('public_pages_index') }}" class="text-sm text-gray-700 underline">Pages</a>--}}
+{{--                <a href="{{ route('public_posts_index') }}" class="text-sm text-gray-700 underline">Posts</a>--}}
+{{--                @auth--}}
+{{--                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>--}}
+{{--                @else--}}
+{{--                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>--}}
+
+{{--                    @if (Route::has('register'))--}}
+{{--                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>--}}
+{{--                    @endif--}}
+{{--                @endauth--}}
+{{--            </div>--}}
         @endif        <!-- Page Heading -->
         @if (isset($header))
             <header class="bg-white shadow">
@@ -42,7 +78,7 @@
                 </div>
             </header>
         @endif
-    </div>
+
 </div>
 <div class="container mx-auto">
     {{ $slot }}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,6 @@
 <x-guest-layout>
-    <h1>Welcome to our site!</h1>
+    <div class="parallax-landing landing">
+        <p class="p1">Leading with love, reaching out for humanity.</p><br/>
+        <p class="p2">Our project aims to create meaningful connections with the homeless community by providing them with basic supplies and a genuine human connection.</p>
+    </div>
 </x-guest-layout>
