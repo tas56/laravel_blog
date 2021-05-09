3a982f51092431a665dbfcf1585b4fd5f70581e3
Thomas Semiz <tas56@njit.edu>
2021-05-09 18:02:51 -0400

    Fully functional for all project requirements

--- resources/views/pages.blade.php
+++ resources/views/pages.blade.php
@@ -1,17 +1,17 @@
 <x-guest-layout>
+    <div class="min-h-screen">
 
-    <div class="parallax-donate donate-header">
+    <div class="parallax-volunteer donate-header">
         <p>Pages</p>
     </div>
     <div class="mt-6 mb-6 content">
+        <ul>
         @foreach ($pages as $page)
             @if($page->published == 1)
-                <div class="border border-black mb-2 p-5" style="max-width: 500px">
-                    <h1 class="text-2xl">{{$page->title}}</h1>
-                    <p>{{$page->user_id}}</p>
-                    <a class="hover:underline" href="{{route('public_pages_show', $page->id)}}">View</a>
-                </div>
+                    <li><a class="text-blue-400 hover:underline mb-5" href="{{route('public_pages_show', $page->id)}}">{{$page->title}}</a></li>
             @endif
         @endforeach
+        </ul>
+    </div>
     </div>
 </x-guest-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,12 +1,12 @@
 <x-guest-layout>
-
+    <div class="min-h-screen">
     <div class="parallax-donate donate-header">
         <p>Blog</p>
     </div>
-    <div class="mt-6 mb-6 content">
+    <div class="mt-6 mb-6 content flex  flex-col">
         @foreach ($posts as $post)
             @if($post->published == 1)
-            <div class="border border-black mb-2 p-5" style="max-width: 500px">
+            <div class="border border-black mb-2 p-5" style="max-width: 900px">
                 <h1 class="text-2xl">{{$post->title}}</h1>
                 <p>{{$post->user_id}}</p>
                 <p class="text-gray-300">{{$post->created_at}}</p>
@@ -14,5 +14,6 @@
             </div>
             @endif
         @endforeach
+    </div>
     </div>
 </x-guest-layout>
